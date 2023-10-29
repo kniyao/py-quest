cbbeef231aa893489e7cb68b3fe933840c68f63d
Niya <niyafathimak1@gmail.com>
2023-10-29 21:21:43 +0530

    You can make a simple  introduction about yourself by typing in the required information

---
+++ introduction.py
@@ -0,0 +1,8 @@
+print("ANSWER THE QUESTIONS BELOW:")
+name = input("What is your name?\n")
+age = input("How old are you?\n")
+profession = input("What is your profession?\n")
+work = input("Where do you work or study?\n")
+hobbies = input("any favorite hobbies?\n")
+
+print("Hello,i'm",name,",a",age,"year old",profession,"at",work,". I enjoy", hobbies)
