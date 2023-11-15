9c0be7d88c8a9a2e9706c161c5929584450b3090
kniyao <thasnik44@gmail.com>
2023-11-15 22:39:00 +0530

    adding tensors together

--- operations/tensor_addition_class.py
+++ operations/tensor_addition_class.py
@@ -3,16 +3,21 @@
 # The objects should be able to be operated upon with numerical operators, such as +, -, *, /.
 
 class Vector:
+
     def __init__(self, vector):
         self.vector = vector
 
+    @classmethod
+    def create_vector(cls, vecData,):
+        return cls(vecData)
+     
     def add(self, vector_to_add):
         addingVec = len(vector_to_add.vector)
         if len(self.vector) == addingVec:
             val = []
             for i in range(addingVec):
                 val.append(self.vector[i] + vector_to_add.vector[i])
-            return Vector(val)
+            return self.create_vector(val)
         else:
             return "Error"
     
@@ -22,7 +27,7 @@ class Vector:
             val = []
             for i in range(subtractingVec):
                 val.append(self.vector[i] - vector_to_subtract.vector[i])
-            return Vector(val)
+            return self.create_vector(val)
         else:
             return "Error"
     
@@ -32,7 +37,7 @@ class Vector:
             val = []
             for i in range(multiplyingVec):
                 val.append(self.vector[i] * vector_to_multiply.vector[i])
-            return Vector(val)
+            return self.create_vector(val)
         else:
             return "Error"
         
@@ -42,18 +47,20 @@ class Vector:
             val = []
             for i in range(dividingVec):
                 val.append(self.vector[i] / vector_to_divide.vector[i])
-            return Vector(val)
+            return self.create_vector(val)
         else:
             return "Error!"
     
+    def __str__(self):
+        return f"{str(self.vector)}"
 
 
         
         
 
 
-v1 = Vector([7,4])
-v2 = Vector([1,3])
+v1 = Vector([8,6])
+v2 = Vector([3,6])
 
-v3 = v1.divide(v2)
-print(v3.vector)
+v3 = v1.add(v2)
+print(v3)
