ba73f7ddfde4f71c6847aa86d1dd4daecd597f8e
kniyao <thasnik44@gmail.com>
2023-11-15 21:34:02 +0530

    How to add two vectors together

---
+++ operations/vector_addition.py
@@ -0,0 +1,21 @@
+# Vector Addition
+
+# Task 1
+# Create a function that takes two vectors as arguments
+# The function should add the values of the vector, and return the result as a vector
+# Eg: [1, 2] + [3, 4] = [4, 6]
+
+def adding_vectors(vec1,vec2):
+    if len(vec1) == len(vec2):
+        result = []
+        for x in range(len(vec1)):
+            result.append(vec1[x] + vec2[x])
+        return result
+    else:
+        return "sorry, The length of the lists should be same!"
+
+v1 = adding_vectors([1,2,3],[1,2,3]) 
+v2 = adding_vectors([5,4,7,8],[5,4,7,8])
+print(v1)
+print(v2)
+
