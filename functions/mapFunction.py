e2c9e0b3354bd729248f8c1c5ee870d112fcd759
kniyao <thasnik44@gmail.com>
2023-11-22 20:43:31 +0530

    how to work with the built in map() function

--- functions/mapFunction.py
+++ functions/mapFunction.py
@@ -23,30 +23,3 @@ test1 = list(map(up,text))
 print(test1)
 
 
-
-#LIST COMPREHENSATION
-"""
-    --> Returns a list based on the values of an existing iterable
-    --> We add a function/another comprehensationlist/ a simple expression as an expression 
-    --> give a placeholder for the item(iterable) 
-    --> We can add condition to the comprehensation
-    --> [*expression* for *item* in *iterable* if *condition*]
-        """
-# example 1
-li = [1,2,3,4,5,6,7,8,9,10]
-
-def fun(x):
-    return x ** 2
-
-list1 = [fun(x) for x in li if x < 5]
-print(list1)
-
-# example 2
-te = "Hello World!"
-
-def upp(tx):
-    return tx.upper()
-
-
-list2 = [upp(tx) for tx in te]
-print(list2)
